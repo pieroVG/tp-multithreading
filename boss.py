9d9c726df90b87dae5241f6ddf9d7226503cc084
Piero Vega <piero.vega-gutierrez@univ-tlse3.fr>
2025-12-17 15:52:18 +0100

    debut tp4 a modif

--- boss.py
+++ boss.py
@@ -1,5 +1,6 @@
 from manager import QueueManager
 from task import Task
+import time
 
 
 def main():
@@ -12,14 +13,19 @@ def main():
     task_queue = manager.get_task_queue()
     result_queue = manager.get_result_queue()
 
+    print("Boss started, manager running")
+
     for i in range(5):
         task_queue.put(Task(identifier=i))
 
+    # Boucle de collecte (exemple)
     for _ in range(5):
         t = result_queue.get()
         print(f"Task {t.identifier} finished in {t.time:.4f}s")
 
-    manager.shutdown()
+    print("Boss waiting (Ctrl+C to stop)")
+    while True:
+        time.sleep(1)
 
 
 if __name__ == "__main__":
